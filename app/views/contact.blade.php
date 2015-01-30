c98a2d1c4d66b8b09edbeabae26ba032f4da3750
ryana <Ryan.Allison@dynamiclogic.com>
2015-01-30 17:31:57 -0500

    Added embedded LinkedIn profile link to Contact page

--- app/views/contact.blade.php
+++ app/views/contact.blade.php
@@ -6,6 +6,10 @@
 <div>
 	<div style="text-align: center; padding: 2em;">
 		<h1>Contact</h1>
+		<br>
+		<br>
+		<script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
+		<script type="IN/MemberProfile" data-id="https://www.linkedin.com/in/RyanAllison1" data-format="inline" data-related="false"></script>
 	</div>
 </div>
 
